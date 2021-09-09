eac82bc4d695f3ad8f924e013b3487a46b40bdd4
Aryaman Mishra <aryamanmishra90@gmail.com>
2021-09-09 23:28:17 +0530

    Thu Sep  9 23:28:17 IST 2021

--- c++Codes/DSA/array/dupicateNum.cpp
+++ c++Codes/DSA/array/dupicateNum.cpp
@@ -4,6 +4,17 @@
 
 using namespace std;
 
+
+int missingNumber(vector<int>& array, int n){
+    int s = 0;
+    for(int i = 0; i < n - 1; i++){
+        s += array[i];
+    }
+    int sum = (n * (n + 1)) / 2;
+    return (sum - s);
+}
+
+
 int findDuplicate(vector<int>& nums){
     for(int i = 0; i < nums.size(); i++){
         if(nums[abs(nums[i])] > 0){
