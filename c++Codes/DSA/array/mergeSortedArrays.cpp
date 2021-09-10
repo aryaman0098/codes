56c994adc5d43e0aa2817d72451d0daf016a5b45
Aryaman Mishra <aryamanmishra90@gmail.com>
2021-09-10 23:24:05 +0530

    Fri 10 Sep 2021 23:24:05 IST

--- c++Codes/DSA/array/mergeSortedArrays.cpp
+++ c++Codes/DSA/array/mergeSortedArrays.cpp
@@ -5,25 +5,36 @@
 using namespace std;
 
 void merge(int arr1[], int arr2[], int n, int m){
-    int i = 0, j = 0;
-    for(; i < n && j < m;){
-        if(arr2[j] < arr1[i]){
-            swap(arr1[i], arr2[j]);
-            i++
-        }else{
-            j++;
-            i++;
+    int gap = ((n + m) / 2) + ((n + m) % 2);
+    while(gap > 0){
+        for(int i = 0, j = gap; j < n + m; i++, j++){
+            if(i < n && j < n){
+                if(arr1[i] > arr1[j]){
+                    swap(arr1[i], arr1[j]);   
+                }
+            }else if(i < n && (n <= j && j < n + m)){
+                if(arr1[i] > arr2[j - n]){
+                    swap(arr1[i], arr2[j - n]);   
+                }
+            }else if((n <= i && i < n + m) && (n <= j && j < n + m)){
+                if(arr2[i - n] > arr2[j - n]){
+                        swap(arr2[i - n], arr2[j - n]);   
+                }
+            }
         }
-    }
-    int min = *min_element(arr2, arr2 + n);
-    int max = *max_element(arr2, arr2 + n);
-
-    while(i < m){
-
+        gap = (gap <= 1) ? 0 : (gap / 2) + (gap % 2);
     }
 }
 
 int main(){
-
+    int arr1[] = {7,9,9,10,11,11,13,14,17,19};
+    int arr2[] = {1,1,4,5,8,11,13,16,19,19};
+    merge(arr1, arr2, 10, 10);
+    for(int i = 0; i < 10; i++){
+        cout<<arr1[i]<<" ";
+    }
+    for(int j = 0; j < 10; j++){
+        cout<<arr2[j]<<" ";
+    }
     return 0;
 }
