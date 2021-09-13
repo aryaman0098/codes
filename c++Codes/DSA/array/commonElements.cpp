cb70c7d0f06384a7250b0c286461b2dafd737466
Aryaman Mishra <aryamanmishra90@gmail.com>
2021-09-13 15:53:09 +0530

    Mon Sep 13 15:53:09 IST 2021

--- c++Codes/DSA/array/commonElements.cpp
+++ c++Codes/DSA/array/commonElements.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 
 vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3){
-    unordered_map<int, int>u2;
-    unordered_map<int, int>u3;
+    unordered_map<int, int> u2;
+    unordered_map<int, int> u3;
+    unordered_map<int, int> counted;
     vector<int> ans;
     for(int i = 0; i < n2; i++){
         u2[B[i]] = 1;
@@ -15,10 +16,16 @@ vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3){
         u3[C[i]] = 1;
     }
     for(int i = 0; i < n1; i++){
-        if(u2.find(A[i]) != u2.end() && u3.find(A[i]) != u3.end()){
+        if(u2.find(A[i]) != u2.end() && u3.find(A[i]) != u3.end() && counted.find(A[i]) == counted.end()){
             ans.push_back(A[i]);
+            counted[A[i]] = 1;
         }
     }
+    if(ans.size() == 0){
+        ans.push_back(-1);
+        return ans;
+    }
+    
     return ans;
 }
 
