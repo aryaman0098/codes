be3dd53a5b94ae1caee1e2bc20a1d46399a17db4
Aryaman Mishra <aryamanmishra90@gmail.com>
2021-09-13 15:28:54 +0530

    Mon Sep 13 15:28:54 IST 2021

---
+++ c++Codes/DSA/array/commonElements.cpp
@@ -0,0 +1,28 @@
+//https://practice.geeksforgeeks.org/problems/common-elements1132/1
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+vector<int> commonElements(int A[], int B[], int C[], int n1, int n2, int n3){
+    unordered_map<int, int>u2;
+    unordered_map<int, int>u3;
+    vector<int> ans;
+    for(int i = 0; i < n2; i++){
+        u2[B[i]] = 1;
+    }
+    for(int i = 0; i < n3; i++){
+        u3[C[i]] = 1;
+    }
+    for(int i = 0; i < n1; i++){
+        if(u2.find(A[i]) != u2.end() && u3.find(A[i]) != u3.end()){
+            ans.push_back(A[i]);
+        }
+    }
+    return ans;
+}
+
+int main(){
+
+    return 0;
+}
