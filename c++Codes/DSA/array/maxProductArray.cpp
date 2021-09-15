caa9be4f98eb65a5d9f87ff89f6abbe5ee2aafdb
Aryaman Mishra <aryamanmishra90@gmail.com>
2021-09-15 13:04:42 +0530

    Wed Sep 15 13:04:42 IST 2021

--- c++Codes/DSA/array/maxProductArray.cpp
+++ c++Codes/DSA/array/maxProductArray.cpp
@@ -5,7 +5,27 @@
 using namespace std;
 
 long long maxProduct(vector<int> arr, int n){
+    long long maxAtIndex = 1;
+    long long minAtIndex = 1;
+    long long max = INT_MIN;
+    for long long i = 0; i < n; i++){
+        if(arr[i] == 0){
+            maxAtIndex = 1;
+            minAtIndex = 1;
+        }
+        long long temp = maxAtIndex;
+        maxAtIndex = (arr[i] > arr[i] * maxAtIndex) ? 
+                     ((arr[i] > arr[i] * minAtIndex) ? arr[i] : arr[i] * minAtIndex) : 
+                     ((arr[i] * maxAtIndex > arr[i] * minAtIndex) ? arr[i] * maxAtIndex : arr[i] * minAtIndex);
+        minAtIndex = (arr[i] < arr[i] * temp) ? 
+                     ((arr[i] < arr[i] * minAtIndex) ? arr[i] : arr[i] * minAtIndex) : 
+                     ((arr[i] * temp < arr[i] * minAtIndex) ? arr[i] * temp : arr[i] * minAtIndex);
     
+        if(max < maxAtIndex){
+            max = maxAtIndex;
+        }
+    }
+    return max;
 }
 
 int main(){
