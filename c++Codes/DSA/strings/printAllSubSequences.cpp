b355a7d37c11296dd3f6ac9dbb10a9ba3fa30cdb
aryaman0098 <aryamanmishra90@gmail.com>
2021-10-07 16:13:28 +0530

    Thu  7 Oct 16:13:28 IST 2021

--- c++Codes/DSA/strings/printAllSubSequences.cpp
+++ c++Codes/DSA/strings/printAllSubSequences.cpp
@@ -7,11 +7,16 @@ using namespace std;
 void printAllSubSequnces(string input, string output){
     if(input.empty()){
         cout<<output<<"\n";
+        return;
     }
-    
+    printAllSubSequnces(input.substr(1), output + input[0]);
+    printAllSubSequnces(input.substr(1), output);
 }
 
 int main(){
 
+    string output = "";
+    string input = "abcde";
+    printAllSubSequnces(input, output);
     return 0;
 }
