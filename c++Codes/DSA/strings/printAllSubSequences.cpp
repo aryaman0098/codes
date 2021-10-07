fecd4b79868a6ae6cbc6d35db64194fa2f64b85a
Aryaman Mishra <aryamanmishra90@gmail.com>
2021-10-07 15:56:30 +0530

    Thu Oct  7 15:56:30 IST 2021

---
+++ c++Codes/DSA/strings/printAllSubSequences.cpp
@@ -0,0 +1,17 @@
+//https://www.geeksforgeeks.org/print-subsequences-string/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void printAllSubSequnces(string input, string output){
+    if(input.empty()){
+        cout<<output<<"\n";
+    }
+    
+}
+
+int main(){
+
+    return 0;
+}
