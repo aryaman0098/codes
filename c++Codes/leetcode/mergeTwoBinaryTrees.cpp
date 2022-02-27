a4b65e9646fb477a589dfcac80f1db5ecdf8decd
Aryaman Mishra <aryamanmishra90@gmail.com>
2022-02-27 23:15:04 +0530

    Sun 27 Feb 2022 23:15:04 IST

---
+++ c++Codes/leetcode/mergeTwoBinaryTrees.cpp
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/merge-two-binary-trees/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+    if(root1 == NULL && root2 == NULL)
+        return NULL;
+    else if(root1 == NULL){
+        return root2;
+    }else if(root2 == NULL){
+        return root1;
+    }
+    TreeNode* root = new TreeNode(root1->val + root2->val);
+    root->left = mergeTrees(root1->left, root2->left);
+    root->right = mergeTrees(root1->right, root2->right);
+    return root;
+}
+
+int main(){
+
+    return 0;
+}
---
+++ c++Codes/leetcode/populatingNextRightPointerInEachNode.cpp
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+Node* connect(Node* root) {
+    
+}
+
+int main(){
+
+    return 0;
+}
