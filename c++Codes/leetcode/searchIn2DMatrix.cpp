4138afd10d2bea37b53f12976728f8345515d250
Aryaman Mishra <aryamanmishra90@gmail.com>
2022-03-02 23:41:10 +0530

    Wed  2 Mar 2022 23:41:10 IST

---
+++ c++Codes/leetcode/searchIn2DMatrix.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/search-a-2d-matrix/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int binarySearch(int l, int r, vector<int> nums, int target){
+    if(l > r)
+        return -1;
+
+    int mid = (l + r) / 2;
+    if(nums[mid] == target)
+        return mid;
+    else if(nums[mid] > target)
+        return binarySearch(l, mid - 1, nums, target);
+    else
+        return binarySearch(mid + 1, r, nums, target);
+}
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    for(int i = 0; i < m; i++){
+        if(matrix[i][0] <= target && target <= matrix[i][n - 1]){
+
+        }
+    }
+}
+
+int main(){
+
+    return 0;
+}
