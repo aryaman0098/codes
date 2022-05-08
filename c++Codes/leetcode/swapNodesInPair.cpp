78283e15ddba9f0f23e5f5f0c616049dad8fa8eb
Aryaman Mishra <aryamanmishra90@gmail.com>
2022-05-09 01:25:38 +0530

    Mon  9 May 2022 01:25:38 IST

---
+++ c++Codes/leetcode/swapNodesInPair.cpp
@@ -0,0 +1,47 @@
+//https://leetcode.com/problems/swap-nodes-in-pairs/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* temp;
+        ListNode* prev = NULL;
+        ListNode* currNode = head;
+        ListNode* ans = head->next;
+        while(currNode != NULL && currNode->next != NULL) {
+            temp = currNode->next->next;
+            currNode->next->next = currNode;
+            if(prev == NULL)
+                prev = currNode;
+            else{
+                prev->next = currNode->next;
+                prev = currNode;
+            }
+            currNode->next = temp;
+            currNode = temp;
+        }
+        return ans; 
+    }
+};
+
+
+int main(){
+
+    return 0;
+}
