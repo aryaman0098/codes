989ec572cc68431a744dde650ecf35cb0578a6fe
Aryaman Mishra <56508277+aryaman0098@users.noreply.github.com>
2025-01-06 18:29:48 +0530

    Create 11.container-with-most-water.cpp

---
+++ cpp/leetcode/11.container-with-most-water.cpp
@@ -0,0 +1,30 @@
+</*
+ * @lc app=leetcode id=11 lang=cpp
+ *
+ * [11] Container With Most Water
+ */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+        int ans = INT_MIN;
+        while(l < r) {
+            ans = max(ans, min(height[l], height[r]) * (r - l));
+            if(height[l] > height[r]) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
+      
