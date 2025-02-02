668eb1ee8a86367e69b142b574fb58d7277085a9
Aryaman Mishra <56508277+aryaman0098@users.noreply.github.com>
2025-02-02 23:30:32 +0530

    Update 2202.maximize-the-topmost-element-after-k-moves.cpp

--- cpp/leetcode/2202.maximize-the-topmost-element-after-k-moves.cpp
+++ cpp/leetcode/2202.maximize-the-topmost-element-after-k-moves.cpp
@@ -12,15 +12,15 @@ using namespace std;
 class Solution {
 public:
     int maximumTop(vector<int>& nums, int k) {
-        if( k > 0 && nums.size() == 1) return -1;
+        if( k % 2 != 0 && nums.size() == 1) return -1;
         if(k == 0 || k == 1) return nums[k];
-        int n = (k > nums.size()) ? nums.size() : k;
+        int n = (k > nums.size()) ? nums.size() : k - 1;
         int ans = INT_MIN;
-        for(int i = 0; i < k; i++) {
+        for(int i = 0; i < n; i++) {
             ans = max(ans, nums[i]);
         }
         if(k < nums.size()) {
-            ans = max(ans, nums[k + 1]);
+            ans = max(ans, nums[k]);
         }
         return ans;
     }
