5697c3561819886f6c6c6207c0bb7d7598e30f31
Aryaman Mishra <56508277+aryaman0098@users.noreply.github.com>
2025-04-26 19:58:50 +0530

    Create 1143.longest-common-subsequence.cpp

---
+++ cpp/leetcode/1143.longest-common-subsequence.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1143 lang=cpp
+ *
+ * [1143] Longest Common Subsequence
+ */
+
+// @lc code=start
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length(), m = text2.length();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; i++) {
+                if(text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+// @lc code=end
+
