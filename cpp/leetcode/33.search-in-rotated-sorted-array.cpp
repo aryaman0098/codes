3e63ff5d0b018d358e96b368def582dade684673
Aryaman Mishra <56508277+aryaman0098@users.noreply.github.com>
2025-05-01 19:59:28 +0530

    Create 33.search-in-rotated-sorted-array.cpp

---
+++ cpp/leetcode/33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=33 lang=cpp
+ *
+ * [33] Search in Rotated Sorted Array
+ */
+
+// @lc code=start
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[l] <= nums[mid]) {
+                if(nums[l] <= target && target <= nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            else {
+                if(nums[mid] <= target && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
+// @lc code=end
+
