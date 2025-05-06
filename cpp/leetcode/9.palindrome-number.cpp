c1eb32175509574c43734c315466cb5c1b2d9e2b
Aryaman Mishra <56508277+aryaman0098@users.noreply.github.com>
2025-05-06 19:58:15 +0530

    Create 9.palindrome-number.cpp

---
+++ cpp/leetcode/9.palindrome-number.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=9 lang=cpp
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
+// @lc code=end
+
