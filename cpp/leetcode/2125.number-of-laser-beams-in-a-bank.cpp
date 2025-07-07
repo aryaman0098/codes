2d9d965df53c9ebf6b68aeb4636bc6a58ebdbbcd
Aryaman Mishra <56508277+aryaman0098@users.noreply.github.com>
2025-07-07 19:37:15 +0530

    Create 2125.number-of-laser-beams-in-a-bank.cpp

---
+++ cpp/leetcode/2125.number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=2125 lang=cpp
+ *
+ * [2125] Number of Laser Beams in a Bank
+ */
+
+// @lc code=start
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int ans = 0, prevIndex = INT_MIN, prevCount = INT_MIN, r = bank.size(),
+            c = bank[0].size();
+        for (int i = 0; i < r; i++) {
+            int newCount = 0;
+            for (int j = 0; j < c; j++)
+                if (bank[i][j] == '1')
+                    newCount++;
+            if (newCount > 0) {
+                if (prevIndex != INT_MIN) {
+                    ans += prevCount * newCount;
+                }
+                prevIndex = i;
+                prevCount = newCount;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
